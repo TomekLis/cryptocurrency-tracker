ca0d0f8fe803955acc9e06886ea32aedb2de847c
Tomek <listomek307@gmail.com>
2019-03-31 17:08:29 +0200

    Added back end authorization

---
+++ ViewModels/CredentialsViewModelValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using FluentValidation.Internal;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptocurrencyTracker.ViewModels
+{
+    public class CredentialsViewModelValidator : IValidatorInterceptor
+    {
+
+        readonly string[] properties = new[] { "userName", "password" };
+
+        public ValidationContext BeforeMvcValidation(ControllerContext cc, ValidationContext context)
+        {
+            var newContext = context.Clone(selector: new MemberNameValidatorSelector(properties));
+            return newContext;
+        }
+
+        public ValidationResult AfterMvcValidation(ControllerContext cc, ValidationContext context, ValidationResult result)
+        {
+            return result;
+        }
+    }
+}
